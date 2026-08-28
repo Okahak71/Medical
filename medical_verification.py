4adbe54b294af218dd9d938850b7b5c5dd56d297
Okahak71 <105473860+Okahak71@users.noreply.github.com>
2026-08-28 19:38:31 +0400

    Update medical_verification.py

--- medical_verification.py
+++ medical_verification.py
@@ -137,7 +137,7 @@ async def verify_license(request: VerificationRequest, x_api_key: str = Header(d
         expiration_result = ai_data.get("expiration_date", "-")
 
         if status_result == "NOT FOUND":
-            screenshot_bytes = await page.screenshot(clip={"x" : 342, "y" : 140, "height" : 260, "width" : 650})
+            screenshot_bytes = await page.screenshot(clip={"x" : 342, "y" : 175, "height" : 225, "width" : 600})
             screenshot_b64 = base64.b64encode(screenshot_bytes).decode("utf-8")
         
         return VerificationResponse(
